809d8a1f08e088d243db965ae7dc7e013608d85a
Highland0971 <joy.highland@gmail.com>
2017-11-25 11:17:10 +0800

    Added bottom navigation and InfoHub custom scrollbar example

--- src/view/commonComponent.js
+++ src/view/commonComponent.js
@@ -1,11 +1,136 @@
 import React,{ Component } from 'react';
+import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
 
+import List, {
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+} from 'material-ui/List';
+
 import DoneIcon from 'material-ui-icons/Done';
 import ScheduleIcon from 'material-ui-icons/Schedule';
-import DefaultIcon from 'material-ui-icons/HelpOutline';
 import RequestIcon from 'material-ui-icons/PanTool';
+
+const contentListStyle = theme =>({
+    root:{
+        borderRadius:2,
+        marginTop: theme.spacing.unit,
+        marginBottom: theme.spacing.unit,
+        marginLeft:theme.spacing.unit,
+        marginRight:theme.spacing.unit,
+        boxShadow:'0px 1px 5px 0px rgba(0, 0, 0, 0.2),' +
+        '0px 2px 2px 0px rgba(0, 0, 0, 0.14), ' +
+        '0px 3px 1px -2px rgba(0, 0, 0, 0.12)'
+    },
+});
+
+class _contentListComponent extends Component{
+
+    constructor(props){
+        super(props);
+        this.handleClick = props.onClick;
+    }
+
+    render(){
+        const { classes } = this.props;
+        console.log('ReRender trigered by _contentListComponent');
+        return(
+            <List >
+                {
+                    this.props.items && this.props.items.map(
+                        entry => (
+
+                            <ListItem button key={entry.id}
+                                      onClick={(selection = entry.id) => {
+                                          this.handleClick && this.handleClick(selection);
+                                             }}
+                                      classes = {{root:classes.root}}
+                            >
+
+                                <ListItemIcon>
+                                    {entry.icon}
+                                </ListItemIcon>
+                                <ListItemText primary={entry.primary} secondary={entry.secondary}  />
+                            </ListItem>)
+                    )
+                }
+            </List>
+        );
+    }
+}
+
+export let ContentListComponent = withStyles(contentListStyle)(_contentListComponent);
+
+
+const sideFunctionMenuStyle =  theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        background: theme.palette.background.paper,
+        paddingLeft:0,
+        height:'100%'
+    },
+    focusedItemIndex:{
+        textDecoration:'none',
+        backgroundColor:'rgba(0, 0, 0, 0.12)'
+    },
+    nested: {
+        paddingLeft: theme.spacing.unit * 4,
+    },
+});
+
+class _sideFunctionMenus extends Component {
+
+    constructor(props){
+        super(props);
+        this.menuConfig = props.menuConfig;
+        this.handleClose = props.onClose;
+        this.handleClick = props.onClick;
+
+    }
+
+    handleSelect(selection)
+    {
+        this.handleClick(selection);
+
+    }
+
+    render() {
+        const classes = this.props.classes;
+        console.log('ReRender trigered by sideFunctionMenus');
+        return (
+            <div className={classes.root}>
+                <List >
+                    {
+                        this.menuConfig && this.menuConfig.map(entry => ( <ListItem button key={entry.key}
+                                                                 onClick={
+                                                                     (selection = entry.key) => {
+                                                                         this.handleClick && this.handleClick(selection);
+                                                                         this.handleClose && this.handleClose();
+                                                                     }
+                                                                 }
+                            >
+                                <ListItemIcon>
+                                    {entry.icon}
+                                </ListItemIcon>
+                                <ListItemText primary={entry.caption} />
+                            </ListItem>)
+                        )
+                    }
+                </List>
+            </div>
+        );
+    }
+}
+
+_sideFunctionMenus.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export let SideFunctionMenu =  withStyles(sideFunctionMenuStyle)(_sideFunctionMenus);
+
 
 const tagButtonStyle = theme => ({
     button:{
@@ -17,23 +142,23 @@ const tagButtonStyle = theme => ({
     icon:{
         height:12
     }
-})
+});
 
 function _TagButton (props) {
-    const classes = props.classes
-    let Icon = <div/>
+    const classes = props.classes;
+    let Icon = <div/>;
     switch (props.type){
         case 'done':
-            Icon = <DoneIcon className={classes.icon}/>
+            Icon = <DoneIcon className={classes.icon}/>;
             break;
         case 'pending':
-            Icon = <ScheduleIcon className={classes.icon}/>
+            Icon = <ScheduleIcon className={classes.icon}/>;
             break;
         case 'request':
-            Icon = <RequestIcon className={classes.icon}/>
+            Icon = <RequestIcon className={classes.icon}/>;
             break;
         default:
-            Icon = Icon;
+            break;
     }
     return(
         <Button raised color={props.color} className={classes.button}>
